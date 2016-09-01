c45b90ac0f1acaae4a735946a57523a5f3a214d8
Fernando Lombos <ferlom2000@gmail.com>
2016-09-01 12:54:50 +0200

    Explain Linux Technologies used in project

--- mySinatraDemoApp.rb
+++ mySinatraDemoApp.rb
@@ -10,7 +10,8 @@ require 'sinatra'
 </head>
 <body>
    <h2>Hellow World Sinatra on Windows 10!</h2>
-   <h3>Raw!!!</h3>
+   <h3>We are using Git and Ruby on Windows 10.</h3>
+   <h3>We are using Sinatra to serve the Demo App</h3>
 </body>
 </html>
 "
