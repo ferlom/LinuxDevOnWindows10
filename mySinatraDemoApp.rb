28629dfae919afd578f911613d9360f5842f67c0
ferlom <ferlom@me.com>
2016-09-01 12:49:38 +0200

    Add files via upload

---
+++ mySinatraDemoApp.rb
@@ -0,0 +1,17 @@
+# myapp.rb
+require 'sinatra'
+
+    get '/' do
+"<!DOCTYPE html>
+<html>
+
+<head>
+   <title>Sinatra Windows Demo </title>
+</head>
+<body>
+   <h2>Hellow World Sinatra on Windows 10!</h2>
+   <h3>Raw!!!</h3>
+</body>
+</html>
+"
+end
